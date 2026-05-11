be042cab53ee65e7cefb22958763625140cbeb4a
yuja814 <tkddj814@naver.com>
2026-05-12 05:29:25 +0900

    nav 수정

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,11 +4,11 @@ import { ROUTES } from "../lib/routes";
 export default function Nav() {
   return (
     <nav>
-      <Link href={ROUTES.home}>Home</Link>
-      <Link href={ROUTES.kim}>Kim</Link>
-      <Link href={ROUTES.park}>Park</Link>
-      <Link href={ROUTES.choo}>Choo</Link>
-      <Link href={ROUTES.lim}>Lim</Link>
+      <Link href={ROUTES.home}>Home </Link>
+      <Link href={ROUTES.kim}> Kim </Link>
+      <Link href={ROUTES.park}> Park </Link>
+      <Link href={ROUTES.choo}> Choo </Link>
+      <Link href={ROUTES.lim}> Lim </Link>
     </nav>
   );
 }
